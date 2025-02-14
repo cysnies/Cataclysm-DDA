055259d88466bdf1f76b3b9f4e5022e9bb2ad83d
Bai Shu "Cysnies <cysnies@outlook.com>
2025-02-14 12:59:53 +0800

    fix: add build flag to allocate more memory

--- build-scripts/build-emscripten.sh
+++ build-scripts/build-emscripten.sh
@@ -4,4 +4,7 @@ set -exo pipefail
 emsdk install 3.1.51
 emsdk activate 3.1.51
 
-make -j`nproc` NATIVE=emscripten BACKTRACE=0 TILES=1 TESTS=0 RUNTESTS=0 RELEASE=1 cataclysm-tiles.js
+# make -j`nproc` NATIVE=emscripten BACKTRACE=0 TILES=1 TESTS=0 RUNTESTS=0 RELEASE=1 cataclysm-tiles.js
+
+make -j`nproc` NATIVE=emscripten BACKTRACE=0 TILES=1 TESTS=0 RUNTESTS=0 RELEASE=1 cataclysm-tiles.js \
+    EMCCFLAGS="-s INITIAL_MEMORY=1073741824 -s MAXIMUM_MEMORY=2147483648"
