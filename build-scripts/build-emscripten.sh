7088bc64af93a29c9861700286fc400a0bdc69ed
Bai Shu "Cysnies <cysnies@outlook.com>
2025-02-15 17:12:35 +0800

    Update build-emscripten.sh

--- build-scripts/build-emscripten.sh
+++ build-scripts/build-emscripten.sh
@@ -10,4 +10,4 @@ then
     emsdk activate ccache-git-emscripten-64bit
 fi
 
-make -j`nproc` NATIVE=emscripten BACKTRACE=0 TILES=1 TESTS=0 RUNTESTS=0 RELEASE=1 CCACHE="$CCACHE" LINTJSON=0 cataclysm-tiles.js
+make -j`nproc` NATIVE=emscripten BACKTRACE=0 TILES=1 TESTS=0 RUNTESTS=0 RELEASE=0 CCACHE="$CCACHE" LINTJSON=0 cataclysm-tiles.js
